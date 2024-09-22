5df516761aa3e6c7e4decda674c75c43b7890fb0
Argo-AscioTech <argo@live.ca>
2024-09-22 19:27:37 +0800

    refactor: VideoPlayer 组件

--- src/components/BootstrapBlazor.VideoPlayer/EnumVideoType.cs
+++ src/components/BootstrapBlazor.VideoPlayer/EnumVideoType.cs
@@ -1,8 +1,6 @@
-﻿// ********************************** 
-// Densen Informatica 中讯科技 
-// 作者：Alex Chow
-// e-mail:zhouchuanglin@gmail.com 
-// **********************************
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using System.ComponentModel;
 
@@ -13,73 +11,109 @@ namespace BootstrapBlazor.Components;
 /// </summary>
 public enum EnumVideoType
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("video/ogg")]
     opus,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("video/ogg")]
     ogv,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("video/mp4")]
     mp4,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("video/mp4")]
     mov,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("video/mp4")]
     m4v,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("video/x-matroska")]
     mkv,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("audio/mp4")]
     m4a,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("audio/mpeg")]
     mp3,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("audio/aac")]
     aac,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("audio/x-caf")]
     caf,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("audio/flac")]
     flac,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("audio/ogg")]
     oga,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("audio/wav")]
     wav,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("application/x-mpegURL")]
     m3u8,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("application/dash+xml")]
     mpd,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("image/jpeg")]
     jpg,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("image/jpeg")]
     jpeg,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("image/gif")]
     gif,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("image/png")]
     png,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("image/svg+xml")]
     svg,
+    /// <summary>
+    /// 
+    /// </summary>
     [Description("image/webp")]
     webp,
-
 }
-//11 :   opus: 'video/ogg',
-//12 :   ogv: 'video/ogg',
-//13 :   mp4: 'video/mp4',
-//14 :   mov: 'video/mp4',
-//15 :   m4v: 'video/mp4',
-//16 :   mkv: 'video/x-matroska',
-//17 :   m4a: 'audio/mp4',
-//18 :   mp3: 'audio/mpeg',
-//19 :   aac: 'audio/aac',
-//20 :   caf: 'audio/x-caf',
-//21 :   flac: 'audio/flac',
-//22 :   oga: 'audio/ogg',
-//23 :   wav: 'audio/wav',
-//24 :   m3u8: 'application/x-mpegURL',
-//25 :   mpd: 'application/dash+xml',
-//26 :   jpg: 'image/jpeg',
-//27 :   jpeg: 'image/jpeg',
-//28 :   gif: 'image/gif',
-//29 :   png: 'image/png',
-//30 :   svg: 'image/svg+xml',
-//31 :   webp: 'image/webp'
-
-//     <source src = "rtmp://rtc.qiscus.com:2935/live360p/testing123&testing.mp4" type="rtmp/mp4">
-//  </source>
-
-//    rtmp/mp4
