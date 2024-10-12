66608b51f5c0ea0711df9c2a994a7cecf0ef8ea6
Amos <hzh990094740@outlook.com>
2024-10-12 16:05:43 +0800

    feat(ImageCropper): support set crop shape (#14)
    * feat(ImageCropper): support set crop shape
    
    * chore: 更新样式到最新
    
    * refactor: 重构代码
    
    * feat: 重构 crop replace reset 方法
    
    * refactor: 重构客户端方法
    
    * feat: 增加禁用逻辑
    
    * refactor: 增加 canvas 销毁代码，防止内存泄漏
    
    * refactor: 释放资源
    
    * refactor: 重构 Options 增加文档注释
    
    * refactor: 更新参数默认值减少通讯量
    
    * refactor: 增加 IsRound 参数
    
    ---------
    
    Co-authored-by: Argo Zhang <argo@live.ca>

--- src/components/BootstrapBlazor.ImageCropper/ImageCropper.razor.js
+++ src/components/BootstrapBlazor.ImageCropper/ImageCropper.razor.js
@@ -1,69 +1,130 @@
-﻿let cropper = null;
-let opt = null;
-let inst = null;
-let result = null;
+﻿import Cropper from './cropper.esm.js'
+import Data from '../BootstrapBlazor/modules/data.js'
+import { addLink } from '../BootstrapBlazor/modules/utility.js'
 
-export async function init(instance, element, options) {
-    inst = instance;
-    opt = options;
+export async function init(id, options) {
+    await addLink("./_content/BootstrapBlazor.ImageCropper/cropper.bundle.css");
 
-    var image = element.querySelector("[data-action=image]");
-    result = element.querySelector("[data-action=result]");
-    cropper = new Cropper(image, options);
-
-}
-
-export async function crop() {
-    cropper.crop();
-    let resultData = cropper.getCroppedCanvas();
-    if (result) {
-        result.innerHTML = '';
-        result.appendChild(resultData);
+    const el = document.getElementById(id);
+    if (el === null) {
+        return;
     }
-    //inst.invokeMethodAsync("GetResult", resultDataUrl);
-    return resultData.toDataURL("image/jpeg", 0.8);
+
+    const image = el.querySelector(".bb-cropper-image");
+    const cropper = new Cropper(image, getOptions(options));
+
+    Data.set(id, cropper);
 }
 
-export async function replace(url) {
-    cropper.replace(url);
+const getOptions = op => {
+    const options = {
+        ...(op || {})
+    }
+    if (options.isRound === true) {
+        options.aspectRatio = 1;
+    }
+    return options;
 }
 
-export async function setData(data) {
-    cropper.setData(data);
-}
+export function dispose(id) {
+    const cropper = Data.get(id);
+    Data.remove(id);
 
-export async function setDragMode(mode) {
-    cropper.setDragMode(mode);
-}
-
-export async function rotate() {
-    cropper.rotate(90);
-}
-
-export async function reset() {
-    cropper.reset();
-}
-
-export async function clear() {
-    cropper.clear();
-}
-
-export async function destroy() {
-    cropper.destroy();
-}
-
-export async function enable() {
-    cropper.enable();
-}
-
-export async function disable() {
-    cropper.disable();
-}
-
-export function destroyMod() {
-    if (undefined !== cropper && null !== cropper) {
+    if (cropper != null) {
         cropper.destroy();
-        cropper = null;
-        console.log('destroy');
     }
+}
+
+export function crop(id) {
+    let ret = null;
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        const { isRound } = cropper.options;
+        cropper.crop();
+        let resultData = cropper.getCroppedCanvas();
+        if (isRound) {
+            resultData = getRoundCanvas(resultData);
+        }
+        ret = resultData.toDataURL();
+        resultData = null;
+    }
+    return ret;
+}
+
+export function replace(id, url) {
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        cropper.replace(url);
+    }
+}
+
+export function reset(id) {
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        cropper.reset();
+    }
+}
+
+export function setDragMode(id, mode) {
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        cropper.setDragMode(mode);
+    }
+}
+
+export function rotate(id, angle) {
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        cropper.rotate(angle);
+    }
+}
+
+export function clear(id) {
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        cropper.clear();
+    }
+}
+
+export async function enable(id) {
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        cropper.enable();
+    }
+
+    const el = document.getElementById(id);
+    if (el) {
+        el.classList.remove("disabled");
+    }
+}
+
+export async function disable(id) {
+    const cropper = Data.get(id);
+    if (cropper != null) {
+        cropper.disable();
+    }
+
+    const el = document.getElementById(id);
+    if (el) {
+        el.classList.add("disabled");
+    }
+}
+
+const getRoundCanvas = sourceCanvas => {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var width = sourceCanvas.width;
+    var height = sourceCanvas.height;
+
+    canvas.width = width;
+    canvas.height = height;
+    context.imageSmoothingEnabled = true;
+    context.drawImage(sourceCanvas, 0, 0, width, height);
+    context.globalCompositeOperation = 'destination-in';
+    context.beginPath();
+    context.arc(width / 2, height / 2, Math.min(width, height) / 2, 0, 2 * Math.PI, true);
+    context.fill();
+
+    sourceCanvas = null;
+    return canvas;
 }
---
+++ src/components/BootstrapBlazor.ImageCropper/ImageCropperDragMode.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 拖动模式
+/// </summary>
+public enum ImageCropperDragMode
+{
+    /// <summary>
+    ///  创建一个新的裁剪框
+    /// </summary>
+    Crop,
+
+    /// <summary>
+    /// 画布和图片都可以移动
+    /// </summary>
+    Move,
+
+    /// <summary>
+    /// 未设置
+    /// </summary>
+    None
+}
