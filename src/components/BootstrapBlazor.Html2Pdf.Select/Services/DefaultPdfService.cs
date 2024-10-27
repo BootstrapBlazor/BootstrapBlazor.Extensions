a6cfaeb75667443e32d0193f349416d9cf871897
Argo Zhang <argo@live.ca>
2024-10-27 16:42:41 +0800

    chore(HtmlToPdf): add HtmlToPdf.Select lib (#96)
    * feat(Html2Pdf): Add new impl of Html2Pdf interface
    
    * fix(Html2Pdf): fix proj location
    
    * fix(Html2Pdf): fix extension method name
    
    * chore: 更改 Key 值
    
    * refactor: 代码重构
    
    * chore: bump version 8.0.0
    
    ---------
    
    Co-authored-by: MadLongTom <36219016+MadLongTom@users.noreply.github.com>

---
+++ src/components/BootstrapBlazor.Html2Pdf.Select/Services/DefaultPdfService.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using SelectPdf;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 默认 Html to Pdf 实现 
+/// </summary>
+class DefaultPdfService : IHtml2Pdf
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public Task<byte[]> PdfDataAsync(string url)
+    {
+        var converter = new HtmlToPdf();
+        var doc = converter.ConvertUrl(url);
+        byte[] result = doc.Save();
+        doc.Close();
+        return Task.FromResult(result);
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public Task<Stream> PdfStreamAsync(string url)
+    {
+        var stream = new MemoryStream();
+        var converter = new HtmlToPdf();
+        var doc = converter.ConvertUrl(url);
+        doc.Save(stream);
+        stream.Flush();
+        stream.Position = 0;
+        return Task.FromResult<Stream>(stream);
+    }
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="html">html raw string</param>
+    /// <param name="links"></param>
+    /// <param name="scripts"></param>
+    public Task<byte[]> PdfDataFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
+    {
+        var converter = new HtmlToPdf();
+        var doc = converter.ConvertHtmlString(html);
+        byte[] result = doc.Save();
+        doc.Close();
+        return Task.FromResult(result);
+    }
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="html">html raw string</param>
+    /// <param name="links"></param>
+    /// <param name="scripts"></param>
+    public Task<Stream> PdfStreamFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
+    {
+        var stream = new MemoryStream();
+        var converter = new SelectPdf.HtmlToPdf();
+        var doc = converter.ConvertHtmlString(html);
+        doc.Save(stream);
+        stream.Flush();
+        stream.Position = 0;
+        return Task.FromResult<Stream>(stream);
+    }
+}
