2f32a1d6b976e1e681141ccf1f3aa64d0cd47e40
Argo-AsicoTech <argo@live.ca>
2024-12-16 11:51:18 +0800

    refactor: 恢复脚本增加静态资源逻辑

--- src/components/BootstrapBlazor.WinBox/WinBox.razor.js
+++ src/components/BootstrapBlazor.WinBox/WinBox.razor.js
@@ -1,5 +1,10 @@
 ﻿import './js/winbox.min.js'
 import Data from '../BootstrapBlazor/modules/data.js'
+import { addLink } from "../BootstrapBlazor/modules/utility.js"
+
+export async function init(id) {
+    await addLink('_content/BootstrapBlazor.WinBox/css/winbox.bundle.css')
+}
 
 export function show(id, invoke, option) {
     const el = document.getElementById(id);
