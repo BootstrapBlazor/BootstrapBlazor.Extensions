9497d1fcb98518e5f072f02e7ded4fcde4cff2b4
Argo Zhang <argo@live.ca>
2025-01-15 19:35:17 +0800

    feat: 增加 SmilesDrawerOption 参数

--- src/components/BootstrapBlazor.SmilesDrawer/SmilesDrawerOptions.cs
+++ src/components/BootstrapBlazor.SmilesDrawer/SmilesDrawerOptions.cs
@@ -12,30 +12,6 @@ namespace BootstrapBlazor.Components;
 public record SmilesDrawerOptions
 {
     /// <summary>
-    /// 是否开启紧凑绘图，默认为 false 不开启
-    /// </summary>
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public bool? CompactDrawing { get; set; }
-
-    /// <summary>
-    /// 键的线粗细，默认值 0.6
-    /// </summary>
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public float? BondThickness { get; set; }
-
-    /// <summary>
-    /// 键的平行线间距，默认值 2.7
-    /// </summary>
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public float? BondSpacing { get; set; }
-
-    /// <summary>
-    /// 是否显示末端碳 (CH3)，默认为 false 不显示
-    /// </summary>
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public bool? TerminalCarbons { get; set; }
-
-    /// <summary>
     /// 获得/设置 分子图宽度单位 px
     /// </summary>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
@@ -46,6 +22,96 @@ public record SmilesDrawerOptions
     /// </summary>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public float? Height { get; set; }
+
+    /// <summary>
+    /// 键的线粗细，默认值 0.6
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public float? BondThickness { get; set; }
+
+    /// <summary>
+    /// Bond length，默认值 15
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? BondLength { get; set; }
+
+    /// <summary>
+    /// Short bond length (e.g. double bonds) in percent of bond length deafult value is 0.85
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public float? ShortBondLength { get; set; }
+
+    /// <summary>
+    /// Bond spacing (e.g. space between double bonds) default value 0.85 * 15
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public float? BondSpacing { get; set; }
+
+    /// <summary>
+    /// Atom Visualization default value 'default'
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? AtomVisualization { get; set; }
+
+    /// <summary>
+    /// Large Font Size (in pt for elements) default value 6
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? FontSizeLarge { get; set; }
+
+    /// <summary>
+    /// Small Font Size (in pt for numbers) default value 4
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? FontSizeSmall { get; set; }
+
+    /// <summary>
+    /// Padding default value 20.0
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public float? Padding { get; set; }
+
+    /// <summary>
+    /// Use experimental features default value false
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? Experimental { get; set; }
+
+    /// <summary>
+    /// Show Terminal Carbons (CH3) default value false
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? TerminalCarbons { get; set; }
+
+    /// <summary>
+    /// Show explicit hydrogens default value false
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? ExplicitHydrogens { get; set; }
+
+    /// <summary>
+    /// Overlap sensitivity default value 0.42
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public float? OverlapSensitivity { get; set; }
+
+    /// <summary>
+    /// # of overlap resolution iterations default value 1
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? OverlapResolutionIterations { get; set; }
+
+    /// <summary>
+    /// 是否开启紧凑绘图，默认为 false 不开启
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? CompactDrawing { get; set; }
+
+    /// <summary>
+    /// Draw isometric SMILES if available default value true
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? Isometric { get; set; }
 
     /// <summary>
     /// 获得/设置 主题 默认 null 使用 light 主题
