6794f8fa891acf7ca7a2d58305c100e04261b703
Argo Zhang <argo@live.ca>
2025-01-30 00:45:47 +0800

    feat(OctIcon): add OctIcon component (#279)
    * chore: 更新资产结构
    
    * feat: 增加 OctIcon 图标集合
    
    * test: 增加单元测试
    
    * chore: 增加 OctIcon 项目

--- test/UniTestSvgIcon/UnitTest.cs
+++ test/UniTestSvgIcon/UnitTest.cs
@@ -20,7 +20,7 @@ public partial class UnitTest
         var zipService = provider.GetRequiredService<IZipArchiveService>();
 
         var root = AppContext.BaseDirectory;
-        var downloadFile = Path.Combine(root, "download.zip");
+        var downloadFile = Path.Combine(root, "IconPark", "download.zip");
         Assert.True(File.Exists(downloadFile));
 
         var downloadFolder = Path.Combine(root, "download");
@@ -33,14 +33,14 @@ public partial class UnitTest
         var folder = new DirectoryInfo(downloadFolder);
 
         // 处理 List 文件
-        var iconListFile = Path.Combine(root, "../../../IconParkList.razor");
+        var iconListFile = Path.Combine(root, "../../../IconPark/IconParkList.razor");
         if (File.Exists(iconListFile))
         {
             File.Delete(iconListFile);
         }
 
         // 处理 svg 文件
-        var svgFile = Path.Combine(root, "../../../icon-park.svg");
+        var svgFile = Path.Combine(root, "../../../IconPark/icon-park.svg");
         if (File.Exists(svgFile))
         {
             File.Delete(svgFile);
@@ -81,7 +81,7 @@ public partial class UnitTest
         var zipService = provider.GetRequiredService<IZipArchiveService>();
 
         var root = AppContext.BaseDirectory;
-        var downloadFile = Path.Combine(root, $"{category}.zip");
+        var downloadFile = Path.Combine(root, "AntDesign", $"{category}.zip");
         Assert.True(File.Exists(downloadFile));
 
         var downloadFolder = Path.Combine(root, category);
@@ -94,14 +94,14 @@ public partial class UnitTest
         var folder = new DirectoryInfo(downloadFolder);
 
         // 处理 List 文件
-        var iconListFile = Path.Combine(root, $"../../../AntDesignIconList_{category}.razor");
+        var iconListFile = Path.Combine(root, $"../../../AntDesign/AntDesignIconList_{category}.razor");
         if (File.Exists(iconListFile))
         {
             File.Delete(iconListFile);
         }
 
         // 处理 svg 文件
-        var svgFile = Path.Combine(root, $"../../../{category}.svg");
+        var svgFile = Path.Combine(root, $"../../../AntDesign/{category}.svg");
         if (File.Exists(svgFile))
         {
             File.Delete(svgFile);
@@ -147,7 +147,7 @@ public partial class UnitTest
         var zipService = provider.GetRequiredService<IZipArchiveService>();
 
         var root = AppContext.BaseDirectory;
-        var downloadFile = Path.Combine(root, $"{category}.zip");
+        var downloadFile = Path.Combine(root, "Element", $"{category}.zip");
         Assert.True(File.Exists(downloadFile));
 
         var downloadFolder = Path.Combine(root, category);
@@ -160,14 +160,14 @@ public partial class UnitTest
         var folder = new DirectoryInfo(downloadFolder);
 
         // 处理 List 文件
-        var iconListFile = Path.Combine(root, $"../../../ElementIconList.razor");
+        var iconListFile = Path.Combine(root, $"../../../Element/ElementIconList.razor");
         if (File.Exists(iconListFile))
         {
             File.Delete(iconListFile);
         }
 
         // 处理 svg 文件
-        var svgFile = Path.Combine(root, $"../../../{category}.svg");
+        var svgFile = Path.Combine(root, $"../../../Element/{category}.svg");
         if (File.Exists(svgFile))
         {
             File.Delete(svgFile);
@@ -205,4 +205,74 @@ public partial class UnitTest
 
     [GeneratedRegex("svg\">(.*)</svg>")]
     private static partial Regex SvgRegex();
+
+
+    [Fact]
+    public void OctIcon_Ok()
+    {
+        var services = new ServiceCollection();
+        services.AddBootstrapBlazor();
+        var provider = services.BuildServiceProvider();
+        var zipService = provider.GetRequiredService<IZipArchiveService>();
+
+        var root = AppContext.BaseDirectory;
+        var downloadFile = Path.Combine(root, "Octicon", "octicons.zip");
+        Assert.True(File.Exists(downloadFile));
+
+        var downloadFolder = Path.Combine(root, "octicons");
+        if (Directory.Exists(downloadFolder))
+        {
+            Directory.Delete(downloadFolder, true);
+        }
+        zipService.ExtractToDirectory(downloadFile, downloadFolder, true);
+
+        var folder = new DirectoryInfo(downloadFolder);
+
+        // 处理 List 文件
+        var iconListFile = Path.Combine(root, $"../../../Octicon/OcticonIconList.razor");
+        if (File.Exists(iconListFile))
+        {
+            File.Delete(iconListFile);
+        }
+
+        // 处理 svg 文件
+        var svgFile = Path.Combine(root, $"../../../Octicon/octicon.svg");
+        if (File.Exists(svgFile))
+        {
+            File.Delete(svgFile);
+        }
+        using var listWriter = new StreamWriter(File.OpenWrite(iconListFile));
+        using var writer = new StreamWriter(File.OpenWrite(svgFile));
+        writer.WriteLine("<svg xmlns=\"http://www.w3.org/2000/svg\">");
+        foreach (var icon in folder.EnumerateFiles())
+        {
+            if (!icon.Name.EndsWith("-16.svg"))
+            {
+                continue;
+            }
+            var id = Path.GetFileNameWithoutExtension(icon.Name);
+            using var reader = new StreamReader(icon.OpenRead());
+            var data = reader.ReadToEnd();
+            reader.Close();
+
+            // find <svg
+            var index = data.IndexOf("<svg ");
+            if (index > -1)
+            {
+                data = data[index..];
+            }
+            index = data.IndexOf(">");
+            if (index > -1)
+            {
+                data = data[(index + 1)..];
+            }
+            var target = data.Replace("</svg>", "").Trim();
+            target = $"    <symbol viewBox=\"0 0 16 16\" id=\"{id}\">{target}</symbol>";
+            writer.WriteLine(target);
+
+            listWriter.WriteLine($"<OctIcon Name=\"{id}\"></OctIcon>");
+        }
+        writer.WriteLine("</svg>");
+        writer.Close();
+    }
 }
