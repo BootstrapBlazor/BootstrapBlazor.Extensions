c877a30580b592c3e1cc783c27e8d846747535ea
Argo Zhang <argo@live.ca>
2025-02-22 22:08:50 +0800

    feat(Html2Image): add IHtml2Image package (#333)
    * feat: 增加 BootstrapBlazor.Html2Image 包
    
    * feat: 增加 MethodName 参数
    
    * refactor: 更新内部逻辑
    
    * feat: 增加内部逻辑代码
    
    * chore: 增加 Html2Image 工程
    
    * refactor: 移动位置
    
    * refactor: 代码格式化
    
    * chore: bump version 9.0.0-beta01

---
+++ src/components/BootstrapBlazor.Html2Image/wwwroot/html2image.js
@@ -0,0 +1,26 @@
+﻿import './lib/html-to-image.js'
+
+export async function execute(selector, methodName, options) {
+    let data = null;
+    const el = document.querySelector(selector);
+    if (el) {
+        const fn = methodName === 'toBlob'
+            ? htmlToImage[methodName]
+            : getMethod(options);
+        data = await fn(el, options);
+    }
+    return data;
+}
+
+const getMethod = options => {
+    let ret = "toPng";
+    if (options) {
+        const { methodName } = options;
+        delete options.methodName;
+
+        if (['toPng', 'toJpeg', 'toSvg', 'toCanvas'].find(i => i === methodName)) {
+            ret = methodName;
+        }
+    }
+    return htmlToImage[ret];
+}
