9e8f4014665233d66acbed779cb6dd1bdc0d4972
Argo Zhang <argo@live.ca>
2025-02-24 14:51:09 +0800

    feat: 增加插件支持

--- src/components/BootstrapBlazor.UniverSheet/Components/UniverSheet.razor.cs
+++ src/components/BootstrapBlazor.UniverSheet/Components/UniverSheet.razor.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using Microsoft.AspNetCore.Components;
 
 namespace BootstrapBlazor.Components;
 
@@ -10,6 +11,24 @@ namespace BootstrapBlazor.Components;
 /// </summary>
 public partial class UniverSheet
 {
+    /// <summary>
+    /// 获得/设置 插件集合 默认 null 未设置
+    /// </summary>
+    [Parameter]
+    public Dictionary<string, string>? Plugins { get; set; }
+
+    /// <summary>
+    /// 获得/设置 主题颜色 默认 null 未设置
+    /// </summary>
+    [Parameter]
+    public string? Theme { get; set; }
+
+    /// <summary>
+    /// 获得/设置 语言 默认 null 未设置
+    /// </summary>
+    [Parameter]
+    public string? Lang { get; set; }
+
     private string? ClassString => CssBuilder.Default("bb-univer-sheet w-100 h-100")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
@@ -18,5 +37,5 @@ public partial class UniverSheet
     /// <inheritdoc/>
     /// </summary>
     /// <returns></returns>
-    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop);
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, new { Theme, Lang, Plugins });
 }
