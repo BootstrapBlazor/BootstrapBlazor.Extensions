093f6de8383d944e44e19db120f97220c6329f46
Argo Zhang <argo@live.ca>
2025-03-02 20:07:46 +0800

    refactor: 增加 saveData 方法

--- src/components/BootstrapBlazor.UniverSheet/wwwroot/plugin.js
+++ src/components/BootstrapBlazor.UniverSheet/wwwroot/plugin.js
@@ -27,22 +27,52 @@ export class DefaultPlugin extends Plugin {
 
     }
 
-    receiveData(data) {
+    receiveData(payload) {
+        const { messageName, commandName, data } = payload;
+        if (messageName === null) {
+            if (commandName === 'Init') {
+                this.renderContent(data);
+            }
+            else if (commandName === 'Push' && Array.isArray(data)) {
+                this.renderContent(data);
+            }
+            else if (commandName === 'Save') {
+                return this.saveData();
+            }
+        }
+        return null;
+    }
+
+    renderWorkbook(data) {
+
+    }
+
+    renderContent(data) {
         this._sheet ??= this._dataService.getUniverSheet();
-        const sheetData = data.data;
         const { univerAPI } = this._sheet;
-        const rows = sheetData.length;
+        const rows = data.length;
         let cols = 1;
         if (rows > 0) {
-            cols = sheetData[0].length;
+            cols = data[0].length;
         }
         const range = univerAPI.getActiveWorkbook().getActiveSheet().getRange(0, 0, rows, cols)
-        const defaultData = sheetData.map(d => {
+        const values = data.map(d => {
             return d.map(v => {
                 return { v: v };
             });
         });
-        range.setValues(defaultData);
+        range.setValues(values);
+    }
+
+    saveData() {
+        this._sheet ??= this._dataService.getUniverSheet();
+        const { univerAPI } = this._sheet;
+        const data = univerAPI.getActiveWorkbook().save();
+        return {
+            messageName: null,
+            commandName: 'Save',
+            data: JSON.stringify(data)
+        };
     }
 }
 
