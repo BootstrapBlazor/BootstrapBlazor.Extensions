3b9ec58688aabd0cbb6c21f13698659e175b56b3
Argo Zhang <argo@live.ca>
2025-03-07 09:10:57 +0800

    feat(UniverIcon): add UniverIcon lib (#353)
    * feat: 增加 UniverIcon
    
    * test: 增加单元测试
    
    * chore: 解决方案增加 UniverIcon 工程
    
    * chore: bump version 9.0.1
    
    * test: 更新单元测试

--- test/UniTestSvgIcon/UnitTest.cs
+++ test/UniTestSvgIcon/UnitTest.cs
@@ -1,11 +1,10 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the Apache 2.0 License
-// See the LICENSE file in the project root for more information.
-// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using BootstrapBlazor.Components;
-using Microsoft.Extensions.DependencyInjection;
 using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace UniTestIconPark;
 
@@ -206,7 +205,6 @@ public partial class UnitTest
     [GeneratedRegex("svg\">(.*)</svg>")]
     private static partial Regex SvgRegex();
 
-
     [Fact]
     public void OctIcon_Ok()
     {
@@ -274,5 +272,92 @@ public partial class UnitTest
         }
         writer.WriteLine("</svg>");
         writer.Close();
+    }
+
+    [Fact]
+    public void UniverIcon_Ok()
+    {
+        var services = new ServiceCollection();
+        services.AddBootstrapBlazor();
+        var provider = services.BuildServiceProvider();
+        var zipService = provider.GetRequiredService<IZipArchiveService>();
+
+        var root = AppContext.BaseDirectory;
+        var downloadFile = Path.Combine(root, "Univer", "univer.zip");
+        Assert.True(File.Exists(downloadFile));
+
+        var downloadFolder = Path.Combine(root, "univer-icons");
+        if (Directory.Exists(downloadFolder))
+        {
+            Directory.Delete(downloadFolder, true);
+        }
+        zipService.ExtractToDirectory(downloadFile, downloadFolder, true);
+
+        var folder = new DirectoryInfo(downloadFolder);
+
+        // 处理 List 文件
+        var iconListFile = Path.Combine(root, $"../../../Univer/UniverIconList.razor");
+        if (File.Exists(iconListFile))
+        {
+            File.Delete(iconListFile);
+        }
+
+        // 处理 svg 文件
+        var svgFile = Path.Combine(root, $"../../../Univer/univer.svg");
+        if (File.Exists(svgFile))
+        {
+            File.Delete(svgFile);
+        }
+        using var listWriter = new StreamWriter(File.OpenWrite(iconListFile));
+        using var writer = new StreamWriter(File.OpenWrite(svgFile));
+        writer.WriteLine("<svg xmlns=\"http://www.w3.org/2000/svg\">");
+        foreach (var icon in folder.EnumerateFiles("*.svg", SearchOption.AllDirectories))
+        {
+            var id = Path.GetFileNameWithoutExtension(icon.Name);
+            using var reader = new StreamReader(icon.OpenRead());
+            var data = reader.ReadToEnd();
+            reader.Close();
+
+            // find viewBox
+            var viewBox = FindViewBox(data);
+
+            // find <svg
+            var index = data.IndexOf("<svg ");
+            if (index > -1)
+            {
+                data = data[index..];
+            }
+            index = data.IndexOf('>');
+            if (index > -1)
+            {
+                data = data[(index + 1)..];
+            }
+
+            var target = data.Replace("</svg>", "").Trim();
+            target = target.Replace("fill=\"black\"", "fill=\"currentColor\"");
+            target = target.Replace("stroke=\"black\"", "stroke=\"currentColor\"");
+            target = $"    <symbol viewBox=\"{viewBox}\" id=\"{id}\">{target}</symbol>";
+            writer.WriteLine(target);
+
+            listWriter.WriteLine($"<UniverIcon Name=\"{id}\"></UniverIcon>");
+        }
+        writer.WriteLine("</svg>");
+        writer.Close();
+    }
+
+    private static string FindViewBox(string svg)
+    {
+        var index = svg.IndexOf(" viewBox=\"");
+        if (index > -1)
+        {
+            index += 10;
+            svg = svg[index..];
+        }
+        index = svg.IndexOf('\"');
+        if (index > -1)
+        {
+            svg = svg[..index];
+        }
+        return svg;
     }
 }
