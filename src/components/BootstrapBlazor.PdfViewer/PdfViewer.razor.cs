61b52300853809c8785db282811dd93389a24bb4
Argo Zhang <argo@live.ca>
2025-06-05 19:02:27 +0800

    feat(PdfViewer): add PdfViewer component (#450)
    * refactor: 增加 PdfViewer 组件
    
    * feat: 增加实现代码逻辑
    
    * refactor: 更新脚本逻辑
    
    * chore: bump version 9.0.0-beta01

---
+++ src/components/BootstrapBlazor.PdfViewer/PdfViewer.razor.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// PdfViewer component for displaying PDF files in a Blazor application.
+/// </summary>
+public partial class PdfViewer
+{
+    /// <summary>
+    /// Gets or sets the url for the PDF file to be displayed.
+    /// </summary>
+    [Parameter]
+    public string? Url { get; set; }
+
+    /// <summary>
+    /// Gets or sets the viewer height. Default is null.
+    /// </summary>
+    [Parameter]
+    public string? Height { get; set; }
+
+    private string? ClassString => CssBuilder.Default("bb-pdf-viewer-container")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    private string? StyleString => CssBuilder.Default()
+        .AddClass($"--bb-pdf-viewer-height: {Height};", !string.IsNullOrEmpty(Height))
+        .Build();
+
+    private string? _url;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            _url = Url;
+        }
+
+        if (_url != Url)
+        {
+            _url = Url;
+            await InvokeVoidAsync("loadPdf", Id, _url);
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id);
+}
---
+++ src/components/BootstrapBlazor.PdfViewer/PdfViewer.razor.js
@@ -0,0 +1,34 @@
+﻿import { addLink } from "../BootstrapBlazor/modules/utility.js"
+import Data from "../BootstrapBlazor/modules/data.js"
+
+export async function init(id) {
+    await addLink("./_content/BootstrapBlazor.PdfViewer/pdf-viewer.css");
+
+    const el = document.getElementById(id);
+    const pdfViewer = { el };
+    Data.set(id, pdfViewer);
+
+    const url = el.getAttribute('data-bb-url');
+    loadPdf(id, url);
+}
+
+export function loadPdf(id, url) {
+    const pdfViewer = Data.get(id);
+    const { el } = pdfViewer;
+    if (url) {
+        const { frame } = pdfViewer;
+        const viewer = frame || createFrame(el);
+        viewer.src = url;
+    }
+    else {
+        delete pdfViewer.frame;
+        el.innerHTML = '';
+    }
+}
+
+const createFrame = el => {
+    const frame = document.createElement('iframe');
+    frame.classList.add('bb-pdf-viewer');
+    el.appendChild(frame);
+    return frame;
+}
