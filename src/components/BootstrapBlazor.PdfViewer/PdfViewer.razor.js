ef3d0335800f4edee081fccdab84a149f82d3970
Argo Zhang <argo@live.ca>
2025-06-05 15:52:12 +0800

    refactor: 更新脚本逻辑

--- src/components/BootstrapBlazor.PdfViewer/PdfViewer.razor.js
+++ src/components/BootstrapBlazor.PdfViewer/PdfViewer.razor.js
@@ -1,40 +1,34 @@
-﻿import { addLink, addScript } from "../BootstrapBlazor/modules/utility.js"
+﻿import { addLink } from "../BootstrapBlazor/modules/utility.js"
 import Data from "../BootstrapBlazor/modules/data.js"
 
 export async function init(id) {
     await addLink("./_content/BootstrapBlazor.PdfViewer/pdf-viewer.css");
 
     const el = document.getElementById(id);
-    const url = el.getAttribute('data-bb-url');
     const pdfViewer = { el };
-
-    createFrame(pdfViewer, url);
     Data.set(id, pdfViewer);
 
-    console.log(pdfViewer);
+    const url = el.getAttribute('data-bb-url');
+    loadPdf(id, url);
 }
 
 export function loadPdf(id, url) {
     const pdfViewer = Data.get(id);
-    const { frame } = pdfViewer;
-    if (frame) {
-        frame.src = url;
-    }
-    else {
-        createFrame(pdfViewer, url);
-    }
-}
-
-const createFrame = (pdfViewer, url) => {
     const { el } = pdfViewer;
     if (url) {
-        const frame = document.createElement('iframe');
-        frame.src = url;
-        frame.classList.add('bb-pdf-viewer');
-        el.appendChild(frame);
-        pdfViewer.frame = frame;
+        const { frame } = pdfViewer;
+        const viewer = frame || createFrame(el);
+        viewer.src = url;
+    }
+    else {
+        delete pdfViewer.frame;
+        el.innerHTML = '';
     }
 }
 
-export function dispose(id) {
+const createFrame = el => {
+    const frame = document.createElement('iframe');
+    frame.classList.add('bb-pdf-viewer');
+    el.appendChild(frame);
+    return frame;
 }
