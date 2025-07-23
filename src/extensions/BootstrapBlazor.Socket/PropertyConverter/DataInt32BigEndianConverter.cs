fed78372c526dd15c193731de5b24d43b0d12cf8
Argo Zhang <argo@live.ca>
2025-07-23 18:08:08 +0800

    feat(TcpSocket): add TcpSocket project (#498)
    * feat: 增加 Socket 相关服务
    
    * test: 更新单元测试
    
    * refactor: 更改类命名空间
    
    * chore: 增加命名空间
    
    * chore: 更改 Utility 扩展类

---
+++ src/extensions/BootstrapBlazor.Socket/PropertyConverter/DataInt32BigEndianConverter.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System.Buffers.Binary;
+
+namespace BootstrapBlazor.DataConverters;
+
+/// <summary>
+/// Sokcet 数据转换为 int 数据大端转换器
+/// </summary>
+public class DataInt32BigEndianConverter : IDataPropertyConverter
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="data"></param>
+    public object? Convert(ReadOnlyMemory<byte> data)
+    {
+        var ret = 0;
+        if (data.Length <= 4)
+        {
+            Span<byte> paddedSpan = stackalloc byte[4];
+            data.Span.CopyTo(paddedSpan[(4 - data.Length)..]);
+
+            if (BinaryPrimitives.TryReadInt32BigEndian(paddedSpan, out var v))
+            {
+                ret = v;
+            }
+        }
+        return ret;
+    }
+}
