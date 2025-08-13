6eeafde6b0593b68c674bb5f372d4d07b6ac09bc
Argo Zhang <argo@live.ca>
2025-08-13 16:46:51 +0800

    chore: 增加 TaskDashboard 组件

---
+++ src/components/BootstrapBlazor.Tasks.Dashboard/TaskDashboard.razor.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Task Dashboard Component
+/// </summary>
+public partial class TaskDashboard
+{
+    [Inject, NotNull]
+    private IStringLocalizer<TaskDashboard>? Localizer { get; set; }
+
+    private string? ClassString => CssBuilder.Default("bb-tasks-dashboard")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    private IEnumerable<IScheduler> _schedulers = [];
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        _schedulers = TaskServicesManager.ToList();
+    }
+}
