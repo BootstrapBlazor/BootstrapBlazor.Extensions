6daed5052ead1430533284448aca4108f1bd5251
Argo Zhang <argo@live.ca>
2025-08-13 18:58:02 +0800

    feat: 实现日志功能

--- src/components/BootstrapBlazor.Tasks.Dashboard/TaskDashboard.razor.cs
+++ src/components/BootstrapBlazor.Tasks.Dashboard/TaskDashboard.razor.cs
@@ -5,7 +5,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 
-namespace BootstrapBlazor.Components;
+namespace BootstrapBlazor.Components.Tasks;
 
 /// <summary>
 /// Task Dashboard Component
@@ -14,6 +14,9 @@ public partial class TaskDashboard
 {
     [Inject, NotNull]
     private IStringLocalizer<TaskDashboard>? Localizer { get; set; }
+
+    [Inject, NotNull]
+    private DialogService? DialogService { get; set; }
 
     private string? ClassString => CssBuilder.Default("bb-tasks-dashboard")
         .AddClassFromAttributes(AdditionalAttributes)
@@ -29,5 +32,80 @@ public partial class TaskDashboard
         base.OnParametersSet();
 
         _schedulers = TaskServicesManager.ToList();
+    }
+
+    private static Color GetStatusColor(SchedulerStatus status) => status switch
+    {
+        SchedulerStatus.Running => Color.Success,
+        SchedulerStatus.Ready => Color.Primary,
+        _ => Color.Danger
+    };
+
+    private string FormatStatus(SchedulerStatus status) => status switch
+    {
+        SchedulerStatus.Running => Localizer["SchedulerStatus.Running"],
+        SchedulerStatus.Ready => Localizer["SchedulerStatus.Ready"],
+        _ => Localizer["SchedulerStatus.Disabled"]
+    };
+
+    private static string GetStatusIcon(SchedulerStatus status) => status switch
+    {
+        SchedulerStatus.Running => "fa-solid fa-play-circle",
+        SchedulerStatus.Ready => "fa-solid fa-stop-circle",
+        _ => "fa-solid fa-times-circle"
+    };
+
+    private static Color GetResultColor(TriggerResult result) => result switch
+    {
+        TriggerResult.Running => Color.Primary,
+        TriggerResult.Success => Color.Success,
+        TriggerResult.Cancelled => Color.Dark,
+        TriggerResult.Timeout => Color.Warning,
+        _ => Color.Danger,
+    };
+
+    private string FormatResult(TriggerResult result) => result switch
+    {
+        TriggerResult.Running => Localizer["TriggerResult.Running"],
+        TriggerResult.Success => Localizer["TriggerResult.Success"],
+        TriggerResult.Cancelled => Localizer["TriggerResult.Timeout"],
+        TriggerResult.Timeout => Localizer["TriggerResult.Timeout"],
+        _ => Localizer["TriggerResult.Error"],
+    };
+
+    private static Task OnPause(IScheduler scheduler)
+    {
+        scheduler.Status = SchedulerStatus.Ready;
+        return Task.CompletedTask;
+    }
+
+    private static Task OnRun(IScheduler scheduler)
+    {
+        scheduler.Status = SchedulerStatus.Running;
+        return Task.CompletedTask;
+    }
+
+    private async Task OnLog(IScheduler scheduler)
+    {
+        var option = new DialogOption()
+        {
+            Class = "modal-dialog-task",
+            Title = Localizer["LogDilaogTitle", scheduler.Name],
+            Component = BootstrapDynamicComponent.CreateComponent<TaskInfo>(new Dictionary<string, object?>
+            {
+                [nameof(TaskInfo.Scheduler)] = scheduler,
+                [nameof(TaskInfo.HeaderText)] = Localizer["LogDilaogConsoleHeaderText"].Value
+            })
+        };
+        await DialogService.Show(option);
+    }
+
+    private static bool OnCheckTaskStatus(IScheduler model) => model.Status != SchedulerStatus.Running;
+
+    private static string? FormatDateTime(DateTimeOffset? dateTime) => dateTime?.ToString("yyyy-MM-dd HH:mm:ss");
+
+    private Task OnShowException(Exception ex)
+    {
+        return Task.CompletedTask;
     }
 }
---
+++ src/components/BootstrapBlazor.Tasks.Dashboard/TaskInfo.razor.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components.Tasks;
+
+/// <summary>
+/// TaskInfo 组件
+/// </summary>
+public partial class TaskInfo : IDisposable
+{
+    /// <summary>
+    /// 获得/设置 <see cref="IScheduler"/> 实例
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    [EditorRequired]
+    public IScheduler? Scheduler { get; set; }
+
+    /// <summary>
+    /// 获得/设置 日志窗口标题文本
+    /// </summary>
+    [Parameter]
+    public string? HeaderText { get; set; } 
+
+    private List<ConsoleMessageItem> Messages { get; } = new(24);
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            var sche = TaskServicesManager.Get(Scheduler.Name);
+            if (sche != null)
+            {
+                sche.Triggers.First().PulseCallback = async t => await DispatchMessage(t);
+                await DispatchMessage(sche.Triggers.First());
+            }
+        }
+    }
+
+    private async Task DispatchMessage(ITrigger trigger)
+    {
+        var message = $"Trigger({trigger.GetType().Name}) LastRuntime: {trigger.LastRuntime} Run({trigger.LastResult}) NextRuntime: {trigger.NextRuntime} Elapsed: {trigger.LastRunElapsedTime.TotalSeconds}";
+        Messages.Add(new ConsoleMessageItem()
+        {
+            Message = message
+        });
+        if (Messages.Count > 20)
+        {
+            Messages.RemoveAt(0);
+        }
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            var sche = TaskServicesManager.Get(Scheduler.Name);
+            if (sche != null)
+            {
+                sche.Triggers.First().PulseCallback = null;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
