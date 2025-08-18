7222d128b61cb40406467ed7da7d3ee562fc99e4
Argo Zhang <argo@live.ca>
2025-08-18 14:14:44 +0800

    refactor: 更新 EditorUploadFile 类

--- src/components/BootstrapBlazor.SummerNote/Components/Editor/EditorUploadFile.cs
+++ src/components/BootstrapBlazor.SummerNote/Components/Editor/EditorUploadFile.cs
@@ -5,111 +5,32 @@
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// 文件信息
+/// Editor 上传文件信息类
 /// </summary>
-public class EditorUploadFile
+public class EditorUploadFile(string fileName, string contentType, long fileSize, Stream stream)
 {
     /// <summary>
-    /// 文件名
+    /// 获得/设置 文件名
     /// </summary>
-    public string? FileName { get; set; }
+    public string FileName => fileName;
 
     /// <summary>
-    /// 文件大小
+    /// 获得/设置 文件大小
     /// </summary>
-    public long FileSize { get; set; }
+    public long FileSize => fileSize;
 
     /// <summary>
-    /// 最后修改日期
+    /// 获得/设置 文件类型
     /// </summary>
-    public string? LastModified { get; set; }
+    public string ContentType => contentType;
 
     /// <summary>
-    /// 文件类型
+    /// 获得/设置 上传的文件流
     /// </summary>
-    public string? ContentType { get; set; }
+    public Stream Stream => stream;
 
     /// <summary>
-    /// 上传的文件流
+    /// 获得/设置 错误信息
     /// </summary>
-    public Stream? UploadStream { get; set; }
-
-    /// <summary>
-    /// 文件索引
-    /// <para>用于标识文件在上传列表中的位置</para>
-    /// <para>例如在上传多个文件时可以通过此属性区分每个文件的索引位置</para>
-    /// </summary>
-    public int Index { get; set; }
-
-    /// <summary>
-    /// 返回码，0为成功，非0失败
-    /// </summary>
-    public int Code { get; set; }
-
-    /// <summary>
-    /// 错误信息
-    /// </summary>
-    public string? Error { get; set; }
-
-    /// <summary>
-    /// 保存到文件
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <param name="token"></param>
-    /// <returns></returns>
-    public async Task<bool> SaveToFile(string fileName, CancellationToken token = default)
-    {
-        var ret = false;
-        if (UploadStream != null)
-        {
-            // 文件保护，如果文件存在则先删除
-            if (System.IO.File.Exists(fileName))
-            {
-                try
-                {
-                    System.IO.File.Delete(fileName);
-                }
-                catch (Exception ex)
-                {
-                    Code = 1002;
-                    Error = ex.Message;
-                }
-            }
-
-            var folder = Path.GetDirectoryName(fileName);
-            if (!string.IsNullOrEmpty(folder) && !Directory.Exists(folder))
-            {
-                Directory.CreateDirectory(folder);
-            }
-
-            if (Code == 0)
-            {
-                using var uploadFile = File.OpenWrite(fileName);
-
-                try
-                {
-                    // 打开文件流
-                    var stream = UploadStream;
-
-                    var buffer = new byte[4 * 1096];
-                    int bytesRead = 0;
-
-                    // 开始读取文件
-                    while ((bytesRead = await stream.ReadAsync(buffer, token)) > 0)
-                    {
-                        await uploadFile.WriteAsync(buffer.AsMemory(0, bytesRead), token);
-
-                    }
-                    ret = true;
-                }
-                catch (Exception ex)
-                {
-                    Code = 1003;
-                    Error = ex.Message;
-                }
-            }
-        }
-        return ret;
-    }
-
+    public Exception? Error { get; set; }
 }
