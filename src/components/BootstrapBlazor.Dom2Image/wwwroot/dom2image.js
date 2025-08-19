af388c508695ed0abd4d6c7f0bec545e9a875af5
Argo Zhang <argo@live.ca>
2025-08-19 15:54:36 +0800

    chore: 增加脚本

---
+++ src/components/BootstrapBlazor.Dom2Image/wwwroot/dom2image.js
@@ -0,0 +1,23 @@
+﻿import { snapdom } from './lib/snapdom.min.mjs'
+
+export async function getUrl(selector, options = {}) {
+    let data = null;
+    const el = document.querySelector(selector);
+    if (el) {
+        options.embedFonts = true;
+        const result = await snapdom(el, options);
+        data = result.url;
+    }
+    return data;
+}
+
+export async function getStream(selector, options = {}) {
+    let data = null;
+    const el = document.querySelector(selector);
+    if (el) {
+        options.embedFonts = true;
+        const result = await snapdom(el, options);
+        data = result.toBlob();
+    }
+    return data;
+}
