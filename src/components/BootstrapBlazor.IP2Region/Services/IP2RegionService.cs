e91fabfcd320db2fa271fb480a38c1ff0d698e88
Argo Zhang <argo@live.ca>
2025-08-19 13:20:52 +0800

    feat(IPRegion): use async increase load speed (#536)
    * doc: 格式化代码
    
    * feat: 使用异步提高性能
    
    * chore: bump version 9.0.3
    
    * perf: 使用异步提高初始化速度

--- src/components/BootstrapBlazor.IP2Region/Services/IP2RegionService.cs
+++ src/components/BootstrapBlazor.IP2Region/Services/IP2RegionService.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Options;
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// 默认 IP2Region 实现 
+/// 默认 IP2Region 实现
 /// </summary>
 class IP2RegionService : DefaultIpLocatorProvider
 {
@@ -25,26 +25,29 @@ class IP2RegionService : DefaultIpLocatorProvider
         _ipOptions = ipRegionOptions;
         _logger = logger;
 
-        InitSearch();
+        Task.Run(InitSearch, CancellationToken.None).ConfigureAwait(false);
     }
 
     private readonly IOptions<BootstrapBlazorOptions> _options;
     private readonly IOptions<IP2RegionOptions> _ipOptions;
     private readonly ILogger<IP2RegionService> _logger;
+    private readonly TaskCompletionSource _tcs = new();
     private Searcher? _search;
 
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
     /// <param name="ip"></param>
-    protected override Task<string?> LocateByIp(string ip)
+    protected override async Task<string?> LocateByIp(string ip)
     {
+        await _tcs.Task;
+
         string? result = null;
         if (_search != null && _options.Value.WebClientOptions.EnableIpLocator)
         {
             result = _search.Search(ip);
         }
-        return Task.FromResult(result);
+        return result;
     }
 
     private void InitSearch()
@@ -60,9 +63,11 @@ class IP2RegionService : DefaultIpLocatorProvider
         {
             _search = new Searcher(CachePolicy.Content, xdbPath);
             _logger.LogInformation("IP2Region xdb file {dbPath} loaded", xdbPath);
+            _tcs.TrySetResult();
         }
         catch (Exception ex)
         {
+            _tcs.TrySetException(ex);
             _logger.LogError(ex, "IP2Region xdb file path: {dbPath}", xdbPath);
         }
     }
