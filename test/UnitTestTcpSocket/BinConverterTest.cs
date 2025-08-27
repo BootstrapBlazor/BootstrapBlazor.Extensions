9fe0f1ed1fdfdd5e38e6e5d8152328fc3efac9d3
Argo Zhang <argo@live.ca>
2025-08-27 09:25:21 +0800

    feat(Converter): add HexConverter/BinConverter static class (#544)
    * feat: 增加 HexConverter 类
    
    * test: 增加单元测试
    
    * refactor: 更新 HexConveter 文档注释
    
    * feat: 增加 BinConverter 转换器
    
    * doc: 更新文档
    
    * test: 更新单元测试
    
    * chore: bump version 9.0.2

---
+++ test/UnitTestTcpSocket/BinConverterTest.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components.DataConverter;
+
+namespace UnitTestTcpSocket;
+
+public class BinConverterTest
+{
+    [Fact]
+    public void ToHexString_Null()
+    {
+        var actual = BinConverter.ToString(null);
+        Assert.Equal(string.Empty, actual);
+
+        actual = BinConverter.ToString([]);
+        Assert.Equal(string.Empty, actual);
+    }
+
+    [Fact]
+    public void ToBinString_Ok()
+    {
+        var data = new byte[] { 0x1A, 0x02 };
+        var actual = BinConverter.ToString(data);
+        Assert.Equal("00011010-00000010", actual);
+
+        actual = BinConverter.ToString(data, " ");
+        Assert.Equal("00011010 00000010", actual);
+    }
+
+    [Fact]
+    public void ToHexString_Exception()
+    {
+        var data = "00011010-00000010";
+        var ex = Assert.ThrowsAny<ArgumentException>(() => BinConverter.ToBytes(data));
+        Assert.NotNull(ex);
+    }
+
+    [Fact]
+    public void ToBytes_Ok()
+    {
+        var excepted = new byte[] { 0x1A, 0x02 };
+
+        var data = "00011010-00000010";
+        var actual = BinConverter.ToBytes(data, "-");
+        Assert.Equal(excepted, actual);
+
+        data = "00011010 00000010";
+        actual = BinConverter.ToBytes(data, " ");
+        Assert.Equal(excepted, actual);
+
+        data = "0001101000000010";
+        actual = BinConverter.ToBytes(data);
+        Assert.Equal(excepted, actual);
+    }
+}
---
+++ test/UnitTestTcpSocket/HexConverterTest.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components.DataConverter;
+
+namespace UnitTestTcpSocket;
+
+public class HexConverterTest
+{
+    [Fact]
+    public void ToHexString_Null()
+    {
+        var actual = HexConverter.ToString(null);
+        Assert.Equal(string.Empty, actual);
+
+        actual = HexConverter.ToString([]);
+        Assert.Equal(string.Empty, actual);
+    }
+
+    [Fact]
+    public void ToHexString_Exception()
+    {
+        var data = "1A021304FE1";
+        var ex = Assert.ThrowsAny<ArgumentException>(() => HexConverter.ToBytes(data));
+        Assert.NotNull(ex);
+    }
+
+    [Fact]
+    public void ToHexString_Ok()
+    {
+        var data = new byte[] { 0x1A, 0x02, 0x13, 0x04, 0xFE };
+        var actual = HexConverter.ToString(data);
+        Assert.Equal("1A-02-13-04-FE", actual);
+
+        actual = HexConverter.ToString(data, " ");
+        Assert.Equal("1A 02 13 04 FE", actual);
+    }
+
+    [Fact]
+    public void ToBytes_Ok()
+    {
+        var excepted = new byte[] { 0x1A, 0x02, 0x13, 0x04, 0xFE };
+
+        var data = "1A021304FE";
+        var actual = HexConverter.ToBytes(data);
+        Assert.Equal(excepted, actual);
+
+        data = "1A-02-13-04-FE";
+        actual = HexConverter.ToBytes(data, "-");
+        Assert.Equal(excepted, actual);
+
+        data = "1A 02 13 04 FE";
+        actual = HexConverter.ToBytes(data, " ");
+        Assert.Equal(excepted, actual);
+    }
+}
