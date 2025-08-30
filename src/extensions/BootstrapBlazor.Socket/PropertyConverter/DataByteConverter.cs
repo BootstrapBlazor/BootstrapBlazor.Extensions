5c66ef33fb62b5f761ab76966a9ab8b37b6c01c6
Argo Zhang <argo@live.ca>
2025-08-30 18:48:21 +0800

    feat: 增加 Byte 转换器

---
+++ src/extensions/BootstrapBlazor.Socket/PropertyConverter/DataByteConverter.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Socket.DataConverters;
+
+/// <summary>
+/// Sokcet 数据转换为 byte 转换器
+/// </summary>
+public class DataByteConverter : IDataPropertyConverter
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="data"></param>
+    public object? Convert(ReadOnlyMemory<byte> data)
+    {
+        return data.Span[0];
+    }
+}
