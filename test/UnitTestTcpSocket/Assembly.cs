5b8e0ab385dd50ef6cb3395513b0f17f0ef64356
Argo Zhang <argo@live.ca>
2025-09-03 14:04:17 +0800

    chore: 删除 Socket 相关工程

--- test/UnitTestTcpSocket/Assembly.cs
+++
@@ -1,5 +0,0 @@
-﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-// Website: https://www.blazor.zone or https://argozhang.github.io/
-
-[assembly: ExcludeFromCodeCoverage]
