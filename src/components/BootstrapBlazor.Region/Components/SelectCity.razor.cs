f75ae546c0d924eccd26f9b32cb5833529908706
Argo Zhang <argo@live.ca>
2025-10-15 09:45:34 +0800

    feat: 实现城市排列

--- src/components/BootstrapBlazor.Region/Components/SelectCity.razor.cs
+++ src/components/BootstrapBlazor.Region/Components/SelectCity.razor.cs
@@ -26,7 +26,65 @@ public partial class SelectCity
         }
     }
 
-    private HashSet<string> GetProvinces() => RegionService.GetProvinces();
+    private static HashSet<string> GetProvinces()
+    {
+        return
+        [
+            "直辖市",
+            "河北省",
+            "山西省",
+            "辽宁省",
+            "吉林省",
+            "黑龙江省",
+            "江苏省",
+            "浙江省",
+            "安徽省",
+            "福建省",
+            "江西省",
+            "山东省",
+            "河南省",
+            "湖北省",
+            "湖南省",
+            "广东省",
+            "海南省",
+            "四川省",
+            "贵州省",
+            "云南省",
+            "陕西省",
+            "甘肃省",
+            "青海省",
+            "内蒙古自治区",
+            "广西壮族自治区",
+            "西藏自治区",
+            "宁夏回族自治区",
+            "新疆维吾尔自治区",
+            "台湾省",
+            "特别行政区"
+        ];
+    }
 
-    private HashSet<string> GetCities(string proviceName) => RegionService.GetCities(proviceName);
+    private HashSet<string> GetCities(string proviceName)
+    {
+        if (proviceName == "直辖市")
+        {
+            return
+            [
+                "北京市",
+                "天津市",
+                "上海市",
+                "重庆市"
+            ];
+        }
+
+        if (proviceName == "特别行政区")
+        {
+            return
+            [
+                "香港特别行政区",
+                "澳门特别行政区"
+            ];
+        }
+
+        return RegionService.GetCities(proviceName);
+    }
 }
