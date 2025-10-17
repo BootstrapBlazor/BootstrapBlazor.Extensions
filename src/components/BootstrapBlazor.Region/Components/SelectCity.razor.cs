5b0cc8c17500dd5dafae8eb8898c33ff781ed58b
Argo Zhang <argo@live.ca>
2025-10-17 12:24:07 +0800

    refactor: 增加搜索功能

--- src/components/BootstrapBlazor.Region/Components/SelectCity.razor.cs
+++ src/components/BootstrapBlazor.Region/Components/SelectCity.razor.cs
@@ -55,6 +55,27 @@ public partial class SelectCity
         SearchIcon ??= IconTheme.GetIconByKey(ComponentIcons.SelectSearchIcon);
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, new
+    {
+        TriggerSearch = nameof(TriggerSearch)
+    });
+
+    /// <summary>
+    /// 触发过滤方法 由 Javascript 触发
+    /// </summary>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public void TriggerSearch(string v)
+    {
+        _searchText = v.ToUpperInvariant();
+        StateHasChanged();
+    }
+
     private async Task OnClearValue()
     {
         if (IsMultiple)
@@ -67,12 +88,6 @@ public partial class SelectCity
         {
             await OnClearAsync();
         }
-    }
-
-    private void OnClearSearch()
-    {
-        _searchText = null;
-        StateHasChanged();
     }
 
     private RenderFragment RenderCities() => builder =>
@@ -119,42 +134,56 @@ public partial class SelectCity
         }
     }
 
-    private static HashSet<string> GetProvinces()
+    private HashSet<string> GetProvinces()
     {
-        return
-        [
-            "直辖市",
-            "河北省",
-            "山西省",
-            "辽宁省",
-            "吉林省",
-            "黑龙江省",
-            "江苏省",
-            "浙江省",
-            "安徽省",
-            "福建省",
-            "江西省",
-            "山东省",
-            "河南省",
-            "湖北省",
-            "湖南省",
-            "广东省",
-            "海南省",
-            "四川省",
-            "贵州省",
-            "云南省",
-            "陕西省",
-            "甘肃省",
-            "青海省",
-            "内蒙古自治区",
-            "广西壮族自治区",
-            "西藏自治区",
-            "宁夏回族自治区",
-            "新疆维吾尔自治区",
-            "台湾省",
-            "特别行政区"
-        ];
+        if (string.IsNullOrEmpty(_searchText))
+        {
+            return Provinces;
+        }
+
+        // 处理直辖市
+        if (PinYinZXS.Contains(_searchText))
+        {
+            return ["直辖市"];
+        }
+
+        return Provinces;
     }
+
+    private static readonly HashSet<string> PinYinZXS = ["BJ", "TJ", "SH", "CQ"];
+
+    private static readonly HashSet<string> Provinces = [
+        "直辖市",
+        "河北省",
+        "山西省",
+        "辽宁省",
+        "吉林省",
+        "黑龙江省",
+        "江苏省",
+        "浙江省",
+        "安徽省",
+        "福建省",
+        "江西省",
+        "山东省",
+        "河南省",
+        "湖北省",
+        "湖南省",
+        "广东省",
+        "海南省",
+        "四川省",
+        "贵州省",
+        "云南省",
+        "陕西省",
+        "甘肃省",
+        "青海省",
+        "内蒙古自治区",
+        "广西壮族自治区",
+        "西藏自治区",
+        "宁夏回族自治区",
+        "新疆维吾尔自治区",
+        "台湾省",
+        "特别行政区"
+    ];
 
     private static readonly HashSet<string> Municipalities = ["北京市", "天津市", "上海市", "重庆市"];
 
---
+++ src/components/BootstrapBlazor.Region/Components/SelectCity.razor.js
@@ -0,0 +1,53 @@
+﻿import Data from "../../BootstrapBlazor/modules/data.js"
+import EventHandler from "../../BootstrapBlazor/modules/event-handler.js"
+import Input from "../../BootstrapBlazor/modules/input.js"
+import Popover from "../../BootstrapBlazor/modules/base-popover.js"
+
+export function init(id, invoke, options) {
+    const el = document.getElementById(id)
+    if (el === null) {
+        return
+    }
+    const popover = Popover.init(el);
+
+    const searchInput = el.querySelector(".search-text");
+    if (searchInput) {
+        Input.composition(searchInput, async v => {
+            await invoke.invokeMethodAsync(options.triggerSearch, v);
+        });
+    }
+
+    const search = el.querySelector(".dropdown-menu-search .clear-icon");
+    if (search) {
+        EventHandler.on(search, 'click', async e => {
+            searchInput.value = '';
+            await invoke.invokeMethodAsync(options.triggerSearch, '');
+        });
+    }
+
+    Data.set(id, { el, popover, searchInput, search });
+}
+
+export function hide(id) {
+    const region = Data.get(id)
+    const { popover } = region;
+    if (popover) {
+        popover.hide();
+    }
+}
+
+export function dispose(id) {
+    const region = Data.get(id)
+    Data.remove(id)
+
+    const { popover, searchInput, search } = region;
+    if (popover) {
+        Popover.dispose(popover);
+    }
+    if (searchInput) {
+        Input.dispose(searchInput);
+    }
+    if (search) {
+        EventHandler.off(search, 'click');
+    }
+}
