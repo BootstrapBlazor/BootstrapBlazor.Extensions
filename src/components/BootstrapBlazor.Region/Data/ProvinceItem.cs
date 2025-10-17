894c420f2d0423c545d5f8552964a2499fabf744
Argo Zhang <argo@live.ca>
2025-10-17 14:41:19 +0800

    refactor: 增加数据结构

---
+++ src/components/BootstrapBlazor.Region/Data/ProvinceItem.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+readonly record struct ProvinceItem
+{
+    public string Name { get; init; }
+
+    public string PinYin { get; init; }
+
+    public HashSet<CityItem> Cities { get; init; }
+}
