ef7764398b6287b1c18a3eea3ee16a5301c7941e
Argo Zhang <argo@live.ca>
2025-10-18 19:41:28 +0800

    feat: 增加 IPinyinService 服务

---
+++ src/components/BootstrapBlazor.Pinyin/Services/IPinyinService.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 汉字拼音服务接口
+/// </summary>
+public interface IPinyinService
+{
+    /// <summary>
+    /// 判断给定字符串是否包含中文
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    bool IsChinese(string text);
+
+    /// <summary>
+    /// 给定中文字符串，返回拼音首字母字符串
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    string GetFirstLetters(string text);
+}
