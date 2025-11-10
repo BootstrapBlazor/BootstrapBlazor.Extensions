4df88accc9680783182a6c760a6050a8acbe367d
Argo Zhang <argo@live.ca>
2025-11-10 11:31:45 +0800

    chore: 移动 Tools 位置

--- tools/BootstrapBlazor.CssBundler/ArgumentsHelper.cs
+++ tools/BootstrapBlazor.CssBundler/ArgumentsHelper.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
@@ -9,9 +9,9 @@ static class ArgumentsHelper
     public static void PrintHelp()
     {
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("Usage: BootstrapBlazorCssBundler [options]");
+        Console.WriteLine("Usage: BootstrapBlazor.CssBundler [options]");
         Console.WriteLine("Options:");
-        Console.WriteLine("  <bundler.json>        ConfigFileFullPath D:\\Argo\\src\\BootstrapBlazor\\src\\BootstrapBlazor\\bundler.json");
+        Console.WriteLine("  <bundler.json>        ConfigFileFullPath D:\\src\\BootstrapBlazor\\src\\BootstrapBlazor\\bundler.json");
         Console.WriteLine();
         Console.ForegroundColor = ConsoleColor.Gray;
     }
