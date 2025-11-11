4f0b0e4a67025ed31c886c4851e413b8f8c70ea2
Argo Zhang <argo@live.ca>
2025-11-11 13:48:34 +0800

    feat(SelectCity): bump version 9.0.9 (#671)
    * refactor: 重构代码支持搜索功能切换
    
    * refactor: 支持搜索状态切换
    
    * chore: bump version 9.0.9

--- src/components/BootstrapBlazor.Region/Components/SelectCity.razor.js
+++ src/components/BootstrapBlazor.Region/Components/SelectCity.razor.js
@@ -1,13 +1,14 @@
-﻿import Data from "../../BootstrapBlazor/modules/data.js"
+import Data from "../../BootstrapBlazor/modules/data.js"
 import EventHandler from "../../BootstrapBlazor/modules/event-handler.js"
 import Input from "../../BootstrapBlazor/modules/input.js"
 import Popover from "../../BootstrapBlazor/modules/base-popover.js"
 
 export function init(id, invoke, options) {
-    const el = document.getElementById(id)
+    const el = document.getElementById(id);
     if (el === null) {
         return
     }
+
     const popover = Popover.init(el, {
         shownCallback: () => {
             if (searchInput != null) {
@@ -16,6 +17,24 @@ export function init(id, invoke, options) {
         }
     });
 
+    const region = { el, invoke, options, popover };
+    initSearch(region);
+    Data.set(id, region);
+}
+
+export function resetSearch(id, search) {
+    const region = Data.get(id);
+
+    if (search) {
+        initSearch(region)
+    }
+    else {
+        disposeSearch(region);
+    }
+}
+
+const initSearch = region => {
+    const { el, invoke, options } = region;
     const searchInput = el.querySelector(".search-text");
     if (searchInput) {
         Input.composition(searchInput, async v => {
@@ -31,7 +50,18 @@ export function init(id, invoke, options) {
         });
     }
 
-    Data.set(id, { el, popover, searchInput, search });
+    region.searchInput = searchInput;
+    region.search = search;
+}
+
+const disposeSearch = region => {
+    const { searchInput, search } = region;
+    if (searchInput) {
+        Input.dispose(searchInput);
+    }
+    if (search) {
+        EventHandler.off(search, 'click');
+    }
 }
 
 export function hide(id) {
@@ -46,14 +76,10 @@ export function dispose(id) {
     const region = Data.get(id)
     Data.remove(id)
 
-    const { popover, searchInput, search } = region;
+    const { popover } = region;
     if (popover) {
         Popover.dispose(popover);
     }
-    if (searchInput) {
-        Input.dispose(searchInput);
-    }
-    if (search) {
-        EventHandler.off(search, 'click');
-    }
+
+    disposeSearch(region);
 }
