3a10fd519ba3c68ce1189aed23a6973d650f6b59
Argo Zhang <argo@live.ca>
2025-11-18 14:26:37 +0800

    feat: 增加导出 Pdf 方法

--- src/components/BootstrapBlazor.Mermaid/Mermaid.razor.js
+++ src/components/BootstrapBlazor.Mermaid/Mermaid.razor.js
@@ -1,14 +1,20 @@
-﻿import mermaid from './mermaid.min.js';
+import { addScript } from '../BootstrapBlazor/modules/utility.js'
 
 export async function init(id, content) {
+    await addScript('./_content/BootstrapBlazor.Mermaid/mermaid.min.js');
+
+    await render(id, content);
+}
+
+export async function render(id, content) {
     mermaid.initialize({ startOnLoad: false });
-    const render = await mermaid.render(id + '-svg', content);
+    const render = await mermaid.render(`${id}-svg`, content);
     if (render) {
         const el = document.getElementById(id);
-        el.innerText = "";
         el.innerHTML = render.svg;
     }
 }
+
 export function getContent(id) {
     let svgDataUrl = "";
     const el = document.getElementById(id);
@@ -24,4 +30,14 @@ export function getContent(id) {
             }
         }
     }
+}
+
+export function getSvgHtml(id) {
+    const el = document.getElementById(id);
+    const svg = el.querySelector('svg');
+    if (svg) {
+        return svg.outerHTML;
+    }
+
+    return "";
 }
