ad7ebada8489f3189f1800da0a8a17f49e2ea004
Argo Zhang <argo@live.ca>
2025-12-17 10:37:55 +0800

    feat(Chart): add Hidden parameter on ChartDataset

--- src/components/BootstrapBlazor.Chart/Components/Chart/ChartDataset.cs
+++ src/components/BootstrapBlazor.Chart/Components/Chart/ChartDataset.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
@@ -11,6 +11,11 @@ namespace BootstrapBlazor.Components;
 /// </summary>
 public class ChartDataset
 {
+    /// <summary>
+    /// 获得/设置 是否不显示 默认 false
+    /// </summary>
+    public bool Hidden { get; set; }
+
     /// <summary>
     /// 获得/设置 数据集合名称
     /// </summary>
