48ce4c676f9222e5686fe8fd76fdf4c713fab462
Argo Zhang <argo@live.ca>
2026-01-14 11:24:19 +0800

    feat: 增加 ShowBorder 功能

--- src/components/BootstrapBlazor.EmbedPDF/EmbedPDF.razor.cs
+++ src/components/BootstrapBlazor.EmbedPDF/EmbedPDF.razor.cs
@@ -18,5 +18,17 @@ public partial class EmbedPDF
     /// 获得/设置 PDF 组件高度 默认 600px
     /// </summary>
     [Parameter]
-    public string? ViewHeight { get; set; }
+    public string ViewHeight { get; set; } = "600px";
+
+    /// <summary>
+    /// 获得/设置 是否显示外边框
+    /// </summary>
+    [Parameter]
+    public bool ShowBorder { get; set; } = true;
+
+    private string? StyleString => CssBuilder.Default()
+        .AddClass("border: 1px solid var(--bs-border-color); border-radius: var(--bs-border-radius); overflow: hidden;", ShowBorder)
+        .AddClass($"height: {ViewHeight};", !string.IsNullOrEmpty(ViewHeight))
+        .AddStyleFromAttributes(AdditionalAttributes)
+        .Build();
 }
