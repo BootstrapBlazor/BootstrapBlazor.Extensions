3ad5529132810095e37446d9c95b2b6988a7a371
Diego <2248356998@qq.com>
2026-01-15 09:01:54 +0800

    添加复现demo

---
+++ BootstrapBlazorApp1/BootstrapBlazorApp1.Server/Components/Pages/ScriptCheck.razor.cs
@@ -0,0 +1,54 @@
+﻿
+using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor;
+
+public partial class ScriptCheck
+{
+    [Parameter, EditorRequired]
+    public string Input { get; set; } = string.Empty;
+    private string Output { get; set; }
+
+    [Parameter, EditorRequired]
+    public string Script { get; set; }
+    [Parameter, EditorRequired]
+    public EventCallback<string> ScriptChanged { get; set; }
+    private async Task Change(string script)
+    {
+        Script = script;
+        if (ScriptChanged.HasDelegate)
+            await ScriptChanged.InvokeAsync(script);
+    }
+
+    [Parameter]
+    public Func<string, string, Task<string>> GetResult { get; set; }
+
+    private async Task CheckScript()
+    {
+        try
+        {
+            if (GetResult != null)
+            {
+                Output = await GetResult(Input, Script).ConfigureAwait(false);
+            }
+        }
+        catch (Exception ex)
+        {
+            Output = ex.ToString();
+        }
+    }
+
+    private async Task GetDemo(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
+    {
+        Script = OnGetDemo?.Invoke();
+        await Change(Script);
+    }
+    CodeEditor CodeEditor;
+    private  async Task Insert()
+    {
+       await CodeEditor.InsertTextAsync("test1");
+    }
+    [Parameter, EditorRequired]
+    public Func<string> OnGetDemo { get; set; }
+}
