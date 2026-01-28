56064f552dad92f6be22c59a751513f63412bc1e
Argo Zhang <argo@live.ca>
2026-01-28 11:55:26 +0800

    feat(PdfViewer): add ShowToolbar parameter (#913)
    * feat(PdfViewer):  add ShowToolbar parameter
    
    * chore: bump version 10.0.1

--- src/components/BootstrapBlazor.PdfViewer/PdfViewer.razor.cs
+++ src/components/BootstrapBlazor.PdfViewer/PdfViewer.razor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
@@ -7,45 +7,59 @@ using Microsoft.AspNetCore.Components;
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// PdfViewer component for displaying PDF files in a Blazor application.
+/// <para lang="zh">PdfViewer 组件</para>
+/// <para lang="en">PdfViewer component for displaying PDF files in a Blazor application</para>
 /// </summary>
 public partial class PdfViewer
 {
     /// <summary>
-    /// Gets or sets the url for the PDF file to be displayed.
+    /// <para lang="zh">获得/设置 文档 Url 属性</para>
+    /// <para lang="en">Gets or sets the url for the PDF file to be displayed</para>
     /// </summary>
     [Parameter]
     public string? Url { get; set; }
 
     /// <summary>
-    /// Gets or sets the page index of the PDF file.
+    /// <para lang="zh">获得/设置 页码索引</para>
+    /// <para lang="en">Gets or sets the page index of the PDF file</para>
     /// </summary>
     [Parameter]
     public int PageIndex { get; set; }
 
     /// <summary>
-    /// Gets or sets the viewer height. Default is null.
+    /// <para lang="zh">获得/设置 查看器高度</para>
+    /// <para lang="en">Gets or sets the viewer height</para>
     /// </summary>
     [Parameter]
     public string? Height { get; set; }
 
     /// <summary>
-    /// Gets or sets the document loaded event callback.
+    /// <para lang="zh">获得/设置 文档加载完成回调事件</para>
+    /// <para lang="en">Gets or sets the document loaded event callback</para>
     /// </summary>
     [Parameter]
     public Func<Task>? OnLoaded { get; set; }
 
     /// <summary>
-    /// Gets or sets the document loaded event callback.
+    /// <para lang="zh">获得/设置 文档不支持回调事件</para>
+    /// <para lang="en">Gets or sets the document not supported event callback</para>
     /// </summary>
     [Parameter]
     public Func<Task>? NotSupportCallback { get; set; }
 
     /// <summary>
-    /// Gets or sets whether to use Google Docs for PDF rendering. Default is false.
+    /// <para lang="zh">获得/设置 是否使用 Google Docs 渲染 PDF</para>
+    /// <para lang="en">Gets or sets whether to use Google Docs for PDF rendering</para>
     /// </summary>
     [Parameter]
     public bool UseGoogleDocs { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">获得/设置 是否显示工具栏与缩略图侧边栏</para>
+    /// <para lang="en">Gets or sets whether to display toolbar and thumbnail sidebar</para>
+    /// </summary>
+    [Parameter]
+    public bool ShowToolbar { get; set; } = true;
 
     [Inject, NotNull]
     private NavigationManager? NavigationManager { get; set; }
@@ -64,7 +78,6 @@ public partial class PdfViewer
     /// <inheritdoc/>
     /// </summary>
     /// <param name="firstRender"></param>
-    /// <returns></returns>
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
@@ -78,7 +91,6 @@ public partial class PdfViewer
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
-    /// <returns></returns>
     protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, new
     {
         LoadedCallaback = nameof(TriggerOnLoaded),
@@ -94,13 +106,28 @@ public partial class PdfViewer
         }
 
         var uri = NavigationManager.ToAbsoluteUri(url);
-        return $"{uri.AbsoluteUri}#page={PageIndex}";
+        var builder = new UriBuilder(uri.AbsoluteUri);
+        builder.Fragment = BuildFragment(builder);
+        return builder.Uri.ToString();
+    }
+
+    private string BuildFragment(UriBuilder builder)
+    {
+        var fragments = new List<string>();
+        if (PageIndex > 0)
+        {
+            fragments.Add($"page={PageIndex}");
+        }
+        if (!ShowToolbar)
+        {
+            fragments.Add("toolbar=0&navpanes=0");
+        }
+        return string.Join('&', fragments);
     }
 
     /// <summary>
     /// Trigger OnLoaded callback when the PDF document is loaded.
     /// </summary>
-    /// <returns></returns>
     [JSInvokable]
     public async Task TriggerOnLoaded()
     {
@@ -113,7 +140,6 @@ public partial class PdfViewer
     /// <summary>
     /// Trigger NotSupportCallback when the PDF viewer does not support the document.
     /// </summary>
-    /// <returns></returns>
     [JSInvokable]
     public async Task TriggerNotSupportCallback()
     {
