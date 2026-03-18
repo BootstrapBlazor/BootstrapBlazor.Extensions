b257a6cb8b9be67c052eded37d44380934a0dab3
zhaijunlei <276318515@qq.com>
2026-03-18 10:54:34 +0800

    fix(dockview): 修复切换语言后恢复删除的panel多语言显示不对

--- src/components/BootstrapBlazor.DockView/wwwroot/js/dockview-group.js
+++ src/components/BootstrapBlazor.DockView/wwwroot/js/dockview-group.js
@@ -26,14 +26,14 @@ const onAddGroup = group => {
     dockview._inited && observeGroup(group)
 }
 
-const addGroupWithPanel = (dockview, panel, panels, index) => {
+const addGroupWithPanel = (dockview, panel, localPanel, panels, index) => {
     if (panel.groupId) {
         addPanelWidthGroupId(dockview, panel, index)
     }
     else {
         addPanelWidthCreatGroup(dockview, panel, panels)
     }
-    deletePanel(dockview, panel)
+    deletePanel(dockview, localPanel)
 }
 
 const addPanelWidthGroupId = (dockview, panel, index) => {
