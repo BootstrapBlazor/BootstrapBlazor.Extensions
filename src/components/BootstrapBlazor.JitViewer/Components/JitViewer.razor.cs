f9148a79d2e02d09a43e58150e91e196df46c0a8
Argo Zhang <argo@live.ca>
2026-05-20 12:15:23 +0800

    feat: 增加功能

--- src/components/BootstrapBlazor.JitViewer/Components/JitViewer.razor.cs
+++ src/components/BootstrapBlazor.JitViewer/Components/JitViewer.razor.cs
@@ -1,16 +1,91 @@
-﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using System.Globalization;
 
 namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// <para lang="zh">JitViewer 组件</para>
+/// <para lang="en">JitViewer component</para>
 /// </summary>
 [JSModuleAutoLoader("./_content/BootstrapBlazor.JitViewer/Components/JitViewer.razor.js", JSObjectReference = true)]
 public partial class JitViewer
 {
+    /// <summary>
+    /// <para lang="zh">预览文件路径</para>
+    /// <para lang="en">File path for preview</para>
+    /// </summary>
+    [Parameter]
+    public string? File { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">预览文件显示名称</para>
+    /// <para lang="en">File display name for preview</para>
+    /// </summary>
+    [Parameter]
+    public string? FileName { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">组件高度 默认未设置</para>
+    /// <para lang="en">Component height, default is not set</para>
+    /// </summary>
+    [Parameter]
+    public int? Height { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">组件是否显示工具栏 默认 true</para>
+    /// <para lang="en">Whether to show the toolbar, default is true</para>
+    /// </summary>
+    [Parameter]
+    public bool ShowToolbar { get; set; } = true;
+
+    /// <summary>
+    /// <para lang="zh">组件主题 默认 auto 值列表 light/dark/auto</para>
+    /// <para lang="en">Component theme, default is auto value list light/dark/auto</para>
+    /// </summary>
+    [Parameter]
+    public string Theme { get; set; } = "auto";
+
     private string? ClassString => CssBuilder.Default("bb-jit-viewer")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
+
+    private string? _lastFile;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            _lastFile = File;
+        }
+
+        if (_lastFile != File)
+        {
+            _lastFile = File;
+            await InvokeVoidAsync("setFile", Id, File, FileName);
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, new
+    {
+        File = File,
+        FileName = FileName,
+        Height = Height ?? 600,
+        Toolbar = ShowToolbar,
+        Locale = CultureInfo.CurrentUICulture.Name,
+        Theme = Theme
+    });
 }
--- src/components/BootstrapBlazor.JitViewer/Components/JitViewer.razor.js
+++ src/components/BootstrapBlazor.JitViewer/Components/JitViewer.razor.js
@@ -1,29 +1,61 @@
-﻿import '../lib/jit-viewer.min.js';
-import Data from '../../BootstrapBlazor/modules/data.js';
-import { addLink } from '../../BootstrapBlazor/modules/utility.js';
+import { addLink, addScript, getTheme } from '../../BootstrapBlazor/modules/utility.js'
+import Data from '../../BootstrapBlazor/modules/data.js'
+import EventHandler from "../../BootstrapBlazor/modules/event-handler.js"
 
-export async function init(id, invoke) {
-    await addLink('./_content/BootstrapBlazor.JitViewer/lib/jit-viewer.min.css');
+export async function init(id, invoke, options) {
+    await addLink('./_content/BootstrapBlazor.JitViewer/jit-viewer.css');
+    await addScript('./_content/BootstrapBlazor.JitViewer/lib/jit-viewer.min.js');
 
     const el = document.getElementById(id);
     if (el === null) {
         return;
     }
 
-    Data.set(id, {
-        el,
-        invoke
-    });
+    if (options.locale.startsWith('en')) {
+        options.locale = 'en';
+    }
+
+    if (options.theme === 'auto') {
+        options.theme = getTheme();
+    }
 
     const { createViewer } = window.JitViewer;
     const viewer = createViewer({
         target: el,
-        theme: 'light',
-        toolbar: true
+        ...options
     });
     viewer.mount();
+
+    Data.set(id, {
+        el,
+        invoke,
+        viewer
+    });
+
+    const updateTheme = e => viewer.setTheme(e.theme);
+    EventHandler.on(document, 'changed.bb.theme', updateTheme);
+}
+
+export function setFile(id, file, fileName) {
+    const data = Data.get(id);
+    if (data === null) {
+        return;
+    }
+
+    const { viewer } = data;
+    if (viewer) {
+        viewer.setFile(file, fileName);
+    }
 }
 
 export function dispose(id) {
+    const data = Data.get(id);
+    if (data === null) {
+        return;
+    }
 
+    const { viewer } = data;
+    if (viewer) {
+        viewer.destroy();
+    }
 }
