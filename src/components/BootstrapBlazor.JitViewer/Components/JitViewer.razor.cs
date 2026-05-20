ad22454c163531e1f64e42effcfc2e78dfa7c6fa
Argo Zhang <argo@live.ca>
2026-05-20 09:33:26 +0800

    chore: 增加 JitViewer 组件

---
+++ src/components/BootstrapBlazor.JitViewer/Components/JitViewer.razor.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) BootstrapBlazor & Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// <para lang="zh">JitViewer 组件</para>
+/// </summary>
+[JSModuleAutoLoader("./_content/BootstrapBlazor.JitViewer/Components/JitViewer.razor.js", JSObjectReference = true)]
+public partial class JitViewer
+{
+    private string? ClassString => CssBuilder.Default("bb-jit-viewer")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
---
+++ src/components/BootstrapBlazor.JitViewer/Components/JitViewer.razor.js
@@ -0,0 +1,29 @@
+﻿import '../lib/jit-viewer.min.js';
+import Data from '../../BootstrapBlazor/modules/data.js';
+import { addLink } from '../../BootstrapBlazor/modules/utility.js';
+
+export async function init(id, invoke) {
+    await addLink('./_content/BootstrapBlazor.JitViewer/lib/jit-viewer.min.css');
+
+    const el = document.getElementById(id);
+    if (el === null) {
+        return;
+    }
+
+    Data.set(id, {
+        el,
+        invoke
+    });
+
+    const { createViewer } = window.JitViewer;
+    const viewer = createViewer({
+        target: el,
+        theme: 'light',
+        toolbar: true
+    });
+    viewer.mount();
+}
+
+export function dispose(id) {
+
+}
